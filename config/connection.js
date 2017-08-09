62e72b8eb16291a1273a0244eb979a89f3571382
trcka <michael.trcka@ttu.edu>
2017-08-09 09:04:47 -0500

    trying to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	port: process.env.PORT || 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
+	port: 3306,
+	host: 'h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'qiuru0akkn86zlz5',
+	password: 'dg9agr73a3jeajdr',
 	database: 'eatzBurgers'
 });
 
